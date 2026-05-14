1efe4f1f6a7b73860fd4a0de1143fef7df0d2d57
daniship14 <danish.p@prometteur.in>
2026-05-14 20:16:29 +0530

    added cicd

--- -fastapi_project/app/main.py
+++ -fastapi_project/app/main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "FastAPI running"}
+
+@app.get("/cicd-test")
+async def cicd_test():
+    return {
+        "status": "success",
+        "message": "Jenkins CI/CD is working"
+    }
